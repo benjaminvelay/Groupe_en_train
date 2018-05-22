440dfb66bfd547c170a99542952d5634d33f5508
Julien Martin <julienm.contact@gmail.com>
2018-05-22 10:24:24 +0200

    fix db schema and migrations

--- db/migrate/20180521171747_create_bookings.rb
+++ db/migrate/20180521171747_create_bookings.rb
@@ -1,8 +1,8 @@
 class CreateBookings < ActiveRecord::Migration[5.2]
   def change
     create_table :bookings do |t|
-      t.references :trip_id, foreign_key: true
-      t.references :user_id, foreign_key: true
+      t.references :trip, foreign_key: true
+      t.references :user, foreign_key: true
       t.integer :status
 
       t.timestamps
