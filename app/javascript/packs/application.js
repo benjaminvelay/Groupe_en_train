51f7e9987f3a643154566f9de8abadc47326c4e7
Florian HAMEL <f.hamel64@gmail.com>
2018-05-23 12:58:06 +0200

    ajout du js pour le click sur tarif group

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,1 +1,7 @@
 import "bootstrap";
+
+const tarifs = document.getElementById("click-tarif-or");
+tarifs.addEventListener("click", (event) => {
+  event.currentTarget.classList.toggle("click-tarif");
+
+});
