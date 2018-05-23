2e0ee6b415728b132e1d668a940a8137772ef2f5
Florian HAMEL <f.hamel64@gmail.com>
2018-05-23 15:13:49 +0200

    js index price

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,5 +3,17 @@ import "bootstrap";
 const tarifs = document.getElementById("click-tarif-or");
 tarifs.addEventListener("click", (event) => {
   event.currentTarget.classList.toggle("click-tarif");
-
 });
+
+// const romain = document.getElementById("romain");
+// romain.addEventListener("click", (event) => {
+//   console.log(event);
+//   console.log(event.currentTarget);
+// });
+
+
+// const romain = document.getElementById("romain");
+// romain.addEventListener("click", (event) => {
+//   console.log(event);
+//   console.log(event.currentTarget);
+// });
