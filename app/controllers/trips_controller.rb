d607a6a1265c5ece5a68f798928d6b94a33f87db
Julien Martin <julienm.contact@gmail.com>
2018-05-25 17:19:22 +0200

    fix api and home css

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -9,6 +9,11 @@ class TripsController < ApplicationController
     trip_date = (temp_date.to_s + "000").to_i
     @trips = ProposedTrip.search(station_departure, station_arrival, trip_date)
     @trips = @trips.select{|trip| trip.departure_date.day == Time.zone.parse(params['search']['departure_at']).day }
+    if @trips.empty?
+      flash[:alert] = "Please review your search inputs."
+      render "pages/home"
+    else
+    end
   end
 
   def show
