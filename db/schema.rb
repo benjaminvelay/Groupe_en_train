2eefa95d58cbf6304bf28ac40ffb0c3ec694870b
Julien Martin <julienm.contact@gmail.com>
2018-05-25 17:48:23 +0200

    fix css

--- db/schema.rb
+++ db/schema.rb
@@ -18,9 +18,10 @@ ActiveRecord::Schema.define(version: 2018_05_24_130152) do
   create_table "bookings", force: :cascade do |t|
     t.bigint "trip_id"
     t.bigint "user_id"
-    t.integer "state", default: 0
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "state"
+    t.jsonb "payment"
     t.index ["trip_id"], name: "index_bookings_on_trip_id"
     t.index ["user_id"], name: "index_bookings_on_user_id"
   end
